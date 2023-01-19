a487e25cb8035c0d9de8f9aa924d6148b90085cf
Denis Beluchenko <dzibis@gmail.com>
2023-01-19 13:54:12 +0700

    final v1

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,10 @@
-
 import org.junit.jupiter.api.Assertions;
 
 public class Test {
     @org.junit.jupiter.api.Test
     public void testAnswer() {
-Category category=new Category(null);
-category.addCategory("еда,");
-Assertions.assertEquals(100,category.answer("еда", 100));
-
+        Category category = new Category(null);
+        category.addCategory("еда,");
+        Assertions.assertEquals(100, category.answer("еда", 100));
     }
-
 }
